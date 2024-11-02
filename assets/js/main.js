fb7d357c509ef14f00486766c3aefbe8da47eeb0
Saalim Sadman <98661736+SadmanSalim@users.noreply.github.com>
2024-11-03 02:09:53 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 let button = document.getElementById('pmt_btn');
 button.addEventListener('click', function() {
-    let pmt = prompt('Enter Celcius Value');
-    let celcius = Number(pmt);
-    let fahrenheith = (celcius * 9/5)+32;
+    let pmt = prompt('Type 1 and press ok for next step');
+    if(pmt == 1){
+        let pmt2 = prompt('Now Press 2 To  Proceed');
+        if(pmt2 == 2){
+            let pmt3 = prompt('Enter Celcius Value');
+            let celcius = Number(pmt3);
+            let far = (celcius * 9/5)+32;
 
-    if(pmt >= 0){
-        document.write(`<h1 style="color: black; font-size: 100px;">${fahrenheith}</h1>`);
+            if(pmt3 > 1){
+                // document.write(`${far}`);
+                alert(`Fahrenhiet Value is ${far}`);
+
+            }
+            else{
+                alert('Invaild Input Plase Dont Use Negative Values');
+
+            }
+        }
+        else{
+            alert('Invaild Input Follow Instructions Properly');
+        }
+        
+
     }
     else{
-        document.write('<h1>Enter Valid Number</h1>');
+        alert('Invaild Input Follow Instructions Properly');
     }
+    
 })
